84f0dd71c0dad344a75cc655e0beb0d20ad55411
Derek <derek.baker89@gmail.com>
2019-06-03 23:28:44 +0100

    Create recipe component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ const App = () => {
   const APP_ID = 'dca42702'
   const APP_KEY = '7a8f19931f731ffdf97b51f3587d5bbb'
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
-
-  const [counter, setCounter] = useState(0);
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    console.log('Effect has been run');
-  });
+   GetRecipes();
+  }, []);
+
+  const GetRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
 
   return(
     <div className="App">
@@ -20,7 +24,7 @@ const App = () => {
         <input className="search-bar" type="text"  />
         <button className="search-button" type="submit">Search</button>
       </form>
-      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
+      
     </div>
   )
 
