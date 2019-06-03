e2c48da05e5e60d73af7ce3ab9a0006cbfdc0a4d
Derek <derek.baker89@gmail.com>
2019-06-03 23:15:52 +0100

    add search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = 'dca42702'
+  const APP_KEY = '7a8f19931f731ffdf97b51f3587d5bbb'
+
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect has been run');
+  });
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <h1>Hello React</h1>
-      </header>
+      <form className="search-from">
+        <input className="search-bar" type="text"  />
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
-  );
+  )
+
 }
 
 export default App;
